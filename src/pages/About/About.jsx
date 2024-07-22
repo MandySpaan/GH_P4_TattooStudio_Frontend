1a36a684ea0179808640fa1e55c9007840992c01
MandySpaan <mandyspaan94@gmail.com>
2024-07-22 19:38:39 +0200

    feat: connected About to fetch backend services

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const About = () => {
-  return <div>About</div>;
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/services");
+        const fetchedData = await response.json();
+        setServices(fetchedData.data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    };
+
+    fetchServices();
+  }, []);
+
+  return (
+    <div>
+      {services.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {services.map((service) => (
+            <li key={service.id}>
+              {service.serviceName} <br />
+              {service.description}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
