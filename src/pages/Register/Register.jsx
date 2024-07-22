6e9bdee1bc4807d8395fd96f37361ea2fbe4a315
MandySpaan <mandyspaan94@gmail.com>
2024-07-22 15:30:07 +0200

    feat: set up Register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Register = () => {
-  return <div>Register</div>;
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    console.log("Handle Change");
+
+    setCredentials((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  console.log(credentials);
+
+  return (
+    <>
+      <h1>Register</h1>
+
+      <input
+        type="text"
+        name="email"
+        id=""
+        placeholder="Email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        id=""
+        placeholder="Password"
+        onChange={handleChange}
+      />
+      <input type="button" value="Register" />
+    </>
+  );
 };
