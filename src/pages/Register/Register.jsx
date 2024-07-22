45388fe250bef3ff7ba115051f5af102f90fb093
MandySpaan <mandyspaan94@gmail.com>
2024-07-22 17:41:29 +0200

    feat: connected Register to backend

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,28 +1,46 @@
 import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { registerUser } from "../../services/apiCalls";
 import "./Register.css";
 
 export const Register = () => {
+  const navigate = useNavigate();
+
   const [credentials, setCredentials] = useState({
     email: "",
     password: "",
   });
 
   function handleChange(e) {
-    console.log("Handle Change");
-
     setCredentials((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   }
 
-  console.log(credentials);
+  async function register() {
+    try {
+      console.log(credentials);
+
+      const response = await registerUser(credentials);
+
+      if (response.success) {
+        navigate("/login");
+      } else {
+        alert(response.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="register-page">
       <div className="register-box">
         <h1>Register</h1>
-        <h2>Create an account or login</h2>
+        <h2>
+          Create an account or <NavLink to="/login">login</NavLink>
+        </h2>
 
         <input
           type="text"
@@ -38,7 +56,7 @@ export const Register = () => {
           placeholder="Password"
           onChange={handleChange}
         />
-        <input type="button" value="Register" />
+        <input type="button" value="Register" onClick={register} />
       </div>
     </div>
   );
