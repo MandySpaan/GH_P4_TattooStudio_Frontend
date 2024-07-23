6bd80a7b7ff9e6e3c64a89e42bec4156394234af
MandySpaan <mandyspaan94@gmail.com>
2024-07-23 21:15:33 +0200

    feat: made Navbar change depending on the user being logged in or not

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import hamburgerImg from "../../img/hamburger-icon.png";
 
@@ -6,10 +6,19 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
+  const navigate = useNavigate();
 
   const handleShowNavbar = () => {
     setShowNav(!showNav);
   };
+
+  const passport = JSON.parse(localStorage.getItem("passport"));
+
+  const logOut = () => {
+    localStorage.removeItem("passport");
+    navigate("/login");
+  };
+
   return (
     <nav className="navbar">
       <div className="container">
@@ -21,16 +30,10 @@ const Navbar = () => {
             <img src={hamburgerImg} alt="Hamburger icon" />
           </h4>
         </div>
-        <div className={`nav-elements  ${showNav && "active"}`}>
+        <div className={`nav-elements ${showNav && "active"}`}>
           <ul>
             <li>
               <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/register">Register</NavLink>
             </li>
             <li>
               <NavLink to="/about">About</NavLink>
@@ -38,6 +41,27 @@ const Navbar = () => {
             <li>
               <NavLink to="/appointments">Book appointment</NavLink>
             </li>
+            {passport && passport.token ? (
+              <>
+                <li>
+                  <NavLink to="/profile">Profile</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/login" onClick={logOut}>
+                    Logout
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/login">Login</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/register">Register</NavLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
