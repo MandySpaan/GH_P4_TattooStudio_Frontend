799a6b8343f7c9cbfb2e0520f67f694ef19f52d5
MandySpaan <mandyspaan94@gmail.com>
2024-07-23 15:37:15 +0200

    feat: set up Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    console.log("HandleChange");
+    setCredentials((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+        <p>
+          Login or <NavLink to="/register">register</NavLink> if you don't have
+          an account yet
+        </p>
+
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <input type="button" value="Register" />
+      </div>
+    </>
+  );
 };
