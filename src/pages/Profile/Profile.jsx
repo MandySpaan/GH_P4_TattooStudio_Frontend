55e7d3dd1222832cda4c67a9f8aa0f6d9dbd110f
MandySpaan <mandyspaan94@gmail.com>
2024-07-24 21:53:02 +0200

    feat: added an AdminArea component to the Profile page which is only visible when logged in with a super_admin token

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import { UserProfile } from "../../components/UserProfile/UserProfile";
 import { MyAppointments } from "../../components/MyAppointments/MyAppointments";
 import "./Profile.css";
+import AdminArea from "../../components/AdminArea/AdminArea";
 
 export const Profile = () => {
+  const passport = JSON.parse(localStorage.getItem("passport")) || {};
+
+  const role = passport.tokenData ? passport.tokenData.role : null;
+
   return (
-    <div className="profile-page">
+    <div div className="profile-page">
+      <div className={role === 1 ? "" : "hidden"}>
+        <AdminArea />
+      </div>
       <div>
         <UserProfile />
       </div>
