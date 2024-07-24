f67f146921fed1c45eb73597a726d8154e266198
MandySpaan <mandyspaan94@gmail.com>
2024-07-24 15:36:30 +0200

    feat: added responsive logo img to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import logo from "../../img/logo.png";
+import "./Home.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-page">
+      <img src={logo} alt="Tattoo Paradise Logo" />
+    </div>
+  );
 };
