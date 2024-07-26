33a5a186151a80d4d65b8b8e22d1c1e605a10279
MandySpaan <mandyspaan94@gmail.com>
2024-07-26 11:21:54 +0200

    feat: added fields to select date and service to BookAppointment

--- src/components/BookAppointment/BookAppointment.jsx
+++ src/components/BookAppointment/BookAppointment.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const BookAppointment = () => {
-  return <div>BookAppointment</div>;
+  const [newAppointment, setNewAppointment] = useState({
+    serviceIs: "",
+    date: "",
+  });
+
+  const todayString = new Date().toISOString().split("T")[0];
+
+  const inputHandler = (e) => {
+    setNewAppointment({
+      ...newAppointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="date"
+        min={todayString}
+        value={newAppointment.date}
+        name="date"
+        onChange={(e) => inputHandler(e)}
+      />
+
+      <select onChange={(e) => inputHandler(e)}>
+        <option disable selected hidden>
+          Please Choose...
+        </option>
+        <option value={1}>Personalized Tattoo</option>
+        <option value={2}>Catalogue Tattoo</option>
+        <option value={3}>Restoration and Rejuvenation</option>
+        <option value={4}>Placement of piercings</option>
+        <option value={5}>The shop</option>
+      </select>
+    </div>
+  );
 };
 
 export default BookAppointment;
