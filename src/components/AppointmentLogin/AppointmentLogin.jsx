ae891986612ef41130bbca02d2c6d18496756ae6
MandySpaan <mandyspaan94@gmail.com>
2024-07-26 19:30:37 +0200

    feat: added css to AppointmentLogin

--- src/components/AppointmentLogin/AppointmentLogin.jsx
+++ src/components/AppointmentLogin/AppointmentLogin.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./AppointmentLogin.css";
 
 const AppointmentLogin = () => {
-  return <div>AppointmentLogin</div>;
+  return (
+    <div className="apptslogin-page">
+      <p>
+        You have to be logged in to be able to make an appointment. <br />
+        Please <NavLink to="/login">login</NavLink> or{" "}
+        <NavLink to="/register">create an account</NavLink>
+      </p>
+    </div>
+  );
 };
 
 export default AppointmentLogin;
